e42efa4a4f87b149507a60168ece58aeae91edb9
Prashareet <prashareet@gmail.com>
2024-11-21 10:51:49 +0530

    trial code of delete todo

--- backend test/index.js
+++ backend test/index.js
@@ -34,6 +34,34 @@ app.get("/todos", function (req, res){
     res.json(todos) //Should respond with all the todos
 })
 
+//Delete test in backend
+
+/*
+    We have to use the app.delete("/todos::id") something like this where our server can access
+    the id and delete it from the backend and send the response back to the frontend.
+
+ */
+//Trial code after learning:
+
+app.delete("/todos/:id", function(req,res){
+    //Very important concept is params.
+    const {id} = req.params.id; //Extracting the id from route parameters.
+    const todoId = parseInt(id,10); // Converting the string to integer
+    const index = todos.findIndex((todo) => todo.id ===todoId); //locating the index of the given id todo
+    //If index not found, return error
+    if(index===-1){
+        // return res.status(400).json("Error : Todo not found"); - Mistake syntax
+        return res.status(404),json({error : "Todo not found"}); //Correct syntax for returning error
+    }
+
+    //Remove the todo from the array:
+    todos.splice(index,1);
+    return res.status(200).json({message : "Todo with id:" + index + " deleted successfully"});
+})
+
+
+
+
 app.listen(3000, () => {
     console.log("Server running at http://localhost:3000");
 });
